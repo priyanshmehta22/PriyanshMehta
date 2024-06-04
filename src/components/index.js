e61149b3d915e16a7d5bf89dd99dac06e9c80403
priyanshmehta22 <73767138+priyanshmehta22@users.noreply.github.com>
2024-06-04 13:55:54 +0530

    update: hero

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { EarthCanvas, BallCanvas, StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -9,11 +9,9 @@ import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
 import Certificates from './Certificates';
-// import MyComputerCanvas from './MyComputerCanvas'
 
 export {
   Hero,
-  // MyComputerCanvas,
   Navbar,
   About,
   Tech,
@@ -25,6 +23,5 @@ export {
   CanvasLoader,
   EarthCanvas,
   BallCanvas,
-  ComputersCanvas,
   StarsCanvas
 };
